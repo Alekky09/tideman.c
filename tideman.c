1be7f3d52493c47e3fc68afbd78469f101a81379
Alekky09 <Alekky09@users.noreply.github.com>
2020-01-12 19:18:34 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -116,7 +116,11 @@ void record_preferences(int ranks[])
 {
     for(int i = 0; i < candidate_count; i++)
     {
-        preferences[ranks[i]][ranks[i + 1]] += 1;
+        for(int j = i + 1; j < candidate_count; j++)
+        {
+            preferences[ranks[i]][ranks[k]]++;
+        }
+
     }
 
     return;
